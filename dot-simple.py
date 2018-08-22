2b53f0c5daa62854c43154a66a05291a797b621a
Kristian Padron <kpadron.github@gmail.com>
2018-08-21 21:11:31 -0400

    changed to tls

--- dot-simple.py
+++ dot-simple.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python3
-import socket
-import requests
-import random
+import socket, ssl
+import struct, random
 
 
 host = '127.0.0.1'
 port = 5053
-headers = {'accept': 'application/dns-message', 'content-type': 'application/dns-message'}
-upstreams = ['https://1.1.1.1/dns-query', 'https://1.0.0.1/dns-query']
+upstreams = ['1.1.1.1']
 conns = []
 
 
@@ -20,7 +18,7 @@ def main():
 	# Connect to upstream servers
 	for upstream in upstreams:
 		print('Connecting to upstream server: %s' % (upstream))
-		conns.append(upstream_connect())
+		conns.append(upstream_connect(upstream, 853, 'cloudflare-dns.com'))
 
 	# Serve forever
 	try:
@@ -29,10 +27,10 @@ def main():
 			data, addr = sock.recvfrom(4096)
 
 			# Select upstream server to forward to
-			index = random.randrange(len(upstreams))
+			index = random.randrange(len(conns))
 
 			# Forward request to upstream server and get response
-			data = upstream_forward(upstreams[index], data, conns[index])
+			data = upstream_forward(data, conns[index])
 
 			# Send response to client
 			sock.sendto(data, addr)
@@ -44,54 +42,77 @@ def main():
 		upstream_close(conn)
 
 	# Close UDP server
-	sock.shutdown(socket.SHUT_RDWR)
 	sock.close()
 
 
-def upstream_connect():
+def upstream_connect(host, port, server_name):
 	"""
-	Create an upstream connection that will later be bound to a url.
-
-	Returns:
-		A requests session object
-	"""
-
-	# Create connection with default DNS message headers
-	session = requests.Session()
-	session.headers = headers
-	return session
-
-
-def upstream_forward(url, data, conn):
-	"""
-	Send a DNS request over HTTPS using POST method.
+	Create an upstream connection to a server.
 
 	Params:
-		url  - url to forward queries to
-		data - normal DNS packet data to forward
-		conn - HTTPS connection to upstream DNS server
+		host        - host server to connect to
+		port        - port on which to connect to host
+		server_name - hostname to use for certificate verification
+
+	Returns:
+		A secure socket object
+	"""
+
+	context = ssl.create_default_context()
+
+	# Create socket connection
+	sock = socket.create_connection((host, port))
+	sock.setsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1)
+	sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPIDLE, 1)
+	sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPINTVL, 3)
+	sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPCNT, 5)
+
+	# Wrap connection with SSL/TLS
+	ssock = context.wrap_socket(sock, server_hostname=server_name)
+	print(ssock.version())
+
+	return ssock
+
+
+def upstream_forward(data, ssock):
+	"""
+	Send a DNS request over TLS.
+
+	Params:
+		data  - normal DNS packet data to forward
+		ssock - SSL/TLS socket connection to upstream DNS server
 
 	Returns:
 		A normal DNS response packet from upstream server
 
 	Notes:
-		Using DNS over HTTPS POST format as described here:
-		https://tools.ietf.org/html/draft-ietf-doh-dns-over-https-12
-		https://developers.cloudflare.com/1.1.1.1/dns-over-https/wireformat/
+		Using DNS over TLS format as described here:
+		https://tools.ietf.org/html/rfc7858
 	"""
 
-	return conn.post(url, data).content
+	# print('sending to upstream: %s' % (data))
+
+	ssock.send(struct.pack('! H', len(data)) + data)
+
+	data = ssock.recv(4096)
+	# print('receiving from upstream: %s' % (data))
+
+	return data[2:]
 
 
-def upstream_close(conn):
+def upstream_close(ssock):
 	"""
 	Close an upstream connection.
 
 	Params:
-		conn - requests session object to close
+		ssock - secure socket object to close
 	"""
 
-	conn.close()
+	# Unwrap secure socket connection
+	sock = ssock.unwrap()
+
+	# Close underlying socket connection
+	sock.close()
 
 
 if __name__ == '__main__':
